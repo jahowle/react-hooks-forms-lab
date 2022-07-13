a777a9dc5525d315d31c814b797fa091a9e91a10
Jonny <jonny.howle@gmail.com>
2022-07-13 15:47:29 -0600

    commit tree 05098b0c23f0b41013294dabee711e59b1cf9a10

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, search, onSearchChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input 
+      type="text" 
+      name="search" 
+      placeholder="Search..." 
+      value={search}
+      onChange={e => onSearchChange(e.target.value)}
+      />
+      <select name="filter" onChange={e => onCategoryChange(e.target.value)}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+
+  const [name, setItemName] = useState("");
+  const [category, setItemCategory] = useState("All");
+
+
+  function handleItemNameChange(event) {
+    setItemName(event.target.value)
+  }
+
+  function handleItemCategoryChange(event) {
+    setItemCategory(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category
+    })
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleItemNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={category} onChange={handleItemCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
